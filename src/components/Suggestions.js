9b2fdbd215cec0fa9a5752286015b96721e42902
Unknown <yash.900209@gmail.com>
2021-05-11 08:53:58 -0400

    removed the nested list

--- src/components/Suggestions.js
+++ src/components/Suggestions.js
@@ -1,12 +1,9 @@
 import React from 'react'
 // Creating the search result by fetching the result object and using it's keys
 const Suggestions = (props) => {
-  //console.log(props.results[0]);
   const options = props.results.map(r => (
-      <ul className="searchResultListParent">
-          <li className="searchResultList">{r[Object.keys(r)[0]]}<span><strong></strong></span></li>
-      </ul>       
+          <li className="searchResultList" onClick={() => console.log(r[Object.keys(r)[0]])}>{r[Object.keys(r)[0]]}<span><strong></strong></span></li>    
   ))
-  return <ul>{options}</ul>
+  return <ul className="searchResultListParent">{options}</ul>
 }
 export default Suggestions
